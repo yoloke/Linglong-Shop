2603aa530e6e4403b037be1cb9d76c5552a58417
SXFreell <xfreell@163.com>
2025-03-30 15:38:15 +0800

    feat: 添加社交媒体图标到页脚，优化样式和布局

--- src/components/PublicFooter/index.vue
+++ src/components/PublicFooter/index.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="footer-container flx-justify-between">
+  <div class="footer-container flx-justify-between select-user-none">
     <span>
       {{ $t("footer.aboutPre") }}
       <a href="https://github.com/OpenAtom-Linyaps/sig-linyaps-simple-store" target="_blank">{{ $t("footer.about") }}</a>
       {{ $t("footer.aboutAft") }}
     </span>
-    <el-tooltip effect="light" placement="top-start">
+    <div class="contact">
+      <span class="text">联系我们</span>
+      <facebook class="icon facebook" />
+      <telegram class="icon telegram" />
+      <wechat class="icon wechat" />
+      <email class="icon email" />
+    </div>
+    <!-- <el-tooltip effect="light" placement="top-start">
       <template #content>
         <div class="flx-column">
           <div class="flx-justify-between info">
@@ -23,10 +30,15 @@
         </div>
       </template>
       <span>{{ $t("footer.developers") }}</span>
-    </el-tooltip>
+    </el-tooltip> -->
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import facebook from "@/assets/icons/facebook.svg?component";
+import telegram from "@/assets/icons/telegram.svg?component";
+import wechat from "@/assets/icons/wechat.svg?component";
+import email from "@/assets/icons/email.svg?component";
+</script>
 <style scoped lang="scss">
 .footer-container {
   position: fixed;
@@ -40,20 +52,51 @@
   background-color: #fff;
   z-index: 9999;
   font-size: 12px;
-  color: #808080;
+  color: #383838;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   a:hover {
-    color: blue;
+    color: #189eff;
   }
-  .info {
-    color: #808080;
-    .item {
-      width: 100%;
-      display: flex;
-      justify-content: space-between;
-      margin-bottom: 8px;
-      background-color: pink;
-      font-size: 22px;
+  .contact {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+    .text {
+      margin-right: 8px;
+    }
+    .icon {
+      cursor: pointer;
+      width: 16px;
+      height: 16px;
+      color: #b6b6b6;
+      &:hover {
+        &.facebook {
+          color: #3c5a99;
+        }
+        &.telegram {
+          color: #1296db;
+        }
+        &.wechat {
+          color: #28c445;
+        }
+        &.email {
+          color: #189eff;
+        }
+      }
     }
   }
+  // .info {
+  //   color: #808080;
+  //   .item {
+  //     width: 100%;
+  //     display: flex;
+  //     justify-content: space-between;
+  //     margin-bottom: 8px;
+  //     background-color: pink;
+  //     font-size: 22px;
+  //   }
+  // }
 }
 </style>
